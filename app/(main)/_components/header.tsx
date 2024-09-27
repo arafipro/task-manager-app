5e2aad447b7df49503b02befb707d206c51400e9
Hiroki Chiyoda <contact@arafipro.com>
2024-09-27 15:27:28 +0900

    07_responsive-header

--- app/(main)/_components/header.tsx
+++ app/(main)/_components/header.tsx
@@ -1,21 +1,38 @@
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
-import { SearchIcon } from "lucide-react";
+import { MoreVertical, SearchIcon } from "lucide-react";
 import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="flex items-center justify-between p-4 shadow-sm">
       <Link href="/">
-        <h1 className="text-2xl font-bold">Task Manager</h1>
+        <h1 className="md:text-2xl font-bold">Task Manager</h1>
       </Link>
       <div className="relative">
         <Input placeholder="検索" className="pl-10" />
         <SearchIcon className="absolute top-1/2 left-3 -translate-y-1/2 text-gray-400" />
       </div>
-      <div className="space-x-4">
+      <div className="space-x-4 hidden md:block">
         <Button>Sign In</Button>
         <Button>Login</Button>
+      </div>
+      <div className="md:hidden">
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <MoreVertical />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem>Sign In</DropdownMenuItem>
+            <DropdownMenuItem>Login</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </header>
   );
